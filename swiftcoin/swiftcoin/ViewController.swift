a50db3bbf38fa4ea72b3867e73cd5e145935d671
Gianni Settino <g@miltonandparc.com>
2015-12-28 14:32:36 -0500

    Create project

---
+++ swiftcoin/swiftcoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftcoin
+//
+//  Created by Gianni Settino on 2015-12-28.
+//  Copyright © 2015 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
